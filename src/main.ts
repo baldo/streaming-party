3b3cdda15db04e3c6b92e0c7ae55c7417274840b
baldo <baldo@hamburg.freifunk.net>
2020-04-06 19:52:17 +0200

    Log version information to console.

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,20 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-import "./plugins"
+import "./plugins";
 import "./components/global";
 
 Vue.config.productionTip = false;
 
+console.log(`
+*** welcome to the streaming party ***
+
+version:\t${process.env.VUE_APP_VERSION}
+git hash:\t${process.env.VUE_APP_GIT_HASH}
+\n`);
+
 new Vue({
   router,
   store,
-  render: h => h(App)
+  render: (h) => h(App),
 }).$mount("#app");
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,12 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+
+const { gitDescribeSync } = require("git-describe");
+const { version } = require("./package.json");
+
+process.env.VUE_APP_VERSION = version;
+process.env.VUE_APP_GIT_HASH = gitDescribeSync().hash;
+
 module.exports = {
   // Allow configuration via .env files.
-  publicPath: process.env.BASE_URL
+  publicPath: process.env.BASE_URL,
 };
